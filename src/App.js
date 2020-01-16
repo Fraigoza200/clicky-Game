c6d1926bcfd212c7b91476acd5712e6ff9c05712
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-01-15 16:04:51 -0800

    Deleted Folders & NavBar

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import DenseAppBar from './components/NavBar'
+
+class App extends Component {
+  render() {
+    return(
+      <div>
+        <DenseAppBar />
+      </div>
+    )
+  }
+}
+
+
+export default App;
